30e826e0d97eef45545f97f6a517784333132103
lkl <394023676@qq.com>
2015-10-21 16:14:01 +0800

    coding changes

--- spiders/emailspider/emailspider/settings.py
+++ spiders/emailspider/emailspider/settings.py
@@ -73,9 +73,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'emailspider.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'emailspider.pipelines.EmailspiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
